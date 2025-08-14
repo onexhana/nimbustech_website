0823bea8c6263f3b4c8315a8663c743145357c06
Kang Ha Na <kanghana0124@kangnam.ac.kr>
2025-08-14 10:23:11 +0900

    feat:(PortfolioSection) 로고와 카드 섹션 간격 설정

--- src/components/Portfolio/PortfolioSection.tsx
+++ src/components/Portfolio/PortfolioSection.tsx
@@ -1,8 +1,10 @@
+// 포트폴리오 섹션 컴포넌트
+
 import PartnerLogoSlider from "./PartnerLogoSlider";
 
 export default function PortfolioSection() {
   return (
-    <div style={{ marginTop: "48px" }}>
+    <div style={{ marginTop: "100px" }}>
       <PartnerLogoSlider />
     </div>
   );
