8bbfc04342e506d5683976bfc270c3b89cb10d2b
SOOMIN227 <soomin0207@naver.com>
2025-09-16 16:40:07 +0900

    ㅋ

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -3,8 +3,8 @@ export const portfolioProjects = [
   // 공공 카테고리
   { id: 1, title: "다산콜센터", description: "다산콜센터 업무자동화", category: "공공", image: "/portfolio_photo/공공_다산콜센터.jpg" },
   { id: 2, title: "중구청", description: "중구청 업무자동화", category: "공공", image: "/portfolio_photo/공공_중구청.jpg" },
-  { id: 3, title: "국가정보자원관리원", description: "범정부 정보자원 통합구축", category: "공공", image: "/portfolio_photo/공공_범정부.jpg" },
-  { id: 4, title: "국가정보자원관리원", description: "정보시스템 유지관리 사업", category: "공공", image: "/portfolio_photo/공공_정보시스템.jpg" },
+  { id: 3, title: "국가정보자원관리원", description: "범정부 정보자원 통합구축", category: "공공", image: "/portfolio_photo/공공_범정부.png" },
+  { id: 4, title: "국가정보자원관리원", description: "정보시스템 유지관리 사업", category: "공공", image: "/portfolio_photo/공공_정보시스템.png" },
   { id: 5, title: "국방통합데이터센터", description: "국방데이터센터 구축 사업", category: "공공", image: "/portfolio_photo/공공_국방데이터센터.jpg" },
   { id: 6, title: "국민연금공단", description: "국민연금 차세대 개발", category: "공공", image: "/portfolio_photo/공공_국민연금.jpg" },
    
@@ -17,9 +17,9 @@ export const portfolioProjects = [
   { id: 27, title: "한국투자증권", description: "AIX 서버 운영", category: "금융", image: "/portfolio_photo/금융_한국투자증권.jpg" },
   { id: 28, title: "메트라이프", description: "데이터센터 운영", category: "금융", image: "/portfolio_photo/금융_메트라이프.jpg" },
   { id: 29, title: "DB생명보험", description: "서버 운영", category: "금융", image: "/portfolio_photo/금융_DB생명보험.jpg" },
-  { id: 30, title: "국민카드", description: "클라우드 사업", category: "금융", image: "/portfolio_photo/금융_국민카드.jpg" },
+  { id: 30, title: "국민카드", description: "클라우드 사업", category: "금융", image: "/portfolio_photo/금융_국민카드.png" },
   { id: 31, title: "ACE 손해보험", description: "대응개발", category: "금융", image: "/portfolio_photo/금융_ACE.jpg" },
-  // 일반 카테고리
+  // 일반/제조 카테고리
   { id: 32, title: "삼성전자", description: "RPA 개발 및 운영사업", category: "일반", image: "/portfolio_photo/일반_삼성전자.jpg" },
   { id: 33, title: "태평양물산", description: "RPA 시범사업", category: "일반", image: "/portfolio_photo/일반_태평양물산.jpg" },
   { id: 34, title: "LG", description: "RPA 구축", category: "일반", image: "/portfolio_photo/일반_LG.jpg" },
