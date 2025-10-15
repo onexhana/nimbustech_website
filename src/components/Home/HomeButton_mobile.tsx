d9a2ab70410b3fb15f09d731b98d3c37c04543af
SungTaeHwan <165628425+sth0824@users.noreply.github.com>
2025-10-15 16:18:44 +0900

    홈버튼 선 길이 수정

--- src/components/Home/HomeButton_mobile.tsx
+++ src/components/Home/HomeButton_mobile.tsx
@@ -311,19 +311,19 @@ export default function HomeButtonMobile({ topOffset = '-40vh', marginTopSpacing
       {renderModal()}
       <div className="w-full bg-white border-t border-gray-300" style={{ position: 'relative', top: topOffset, marginTop: marginTopSpacing, marginBottom: marginBottomSpacing }}>
         {/* 모바일용 2x2 그리드 - 동일한 크기로 분할 */}
-        <div style={{ width: '100%', maxWidth: '390px', margin: '0 auto', position: 'relative' }}>
+        <div style={{ width: '100%', maxWidth: '1000px', margin: '0 auto', position: 'relative' }}>
           {/* 내부 세로 구분선 (상단 두 카드 사이) */}
           <div style={{ position: 'absolute', top: 0, left: '50%', bottom: '50%', borderLeft: '1px solid #d1d5db' }} />
           {/* 내부 세로 구분선 (하단 두 카드 사이) */}
           <div style={{ position: 'absolute', top: '50%', left: '50%', bottom: 0, borderLeft: '1px solid #d1d5db' }} />
-          {/* 내부 가로 구분선 (중간) */}
-          <div style={{ position: 'absolute', top: '50%', left: -100, right: -100, borderTop: '1px solid #d1d5db' }} />
+          {/* 내부 가로 구분선 (중간) - 반응형으로 수정 */}
+          <div style={{ position: 'absolute', top: '50%', left: 0, right: 0, borderTop: '1px solid #d1d5db' }} />
           <div style={{ 
             display: 'grid', 
             gridTemplateColumns: '1fr 1fr', 
             gridTemplateRows: '1fr 1fr', 
             width: '100%', 
-            maxWidth: '390px',
+            maxWidth: '1000px',
             height: '100%' 
           }}>
           {homeData.buttonData.map((btn, idx) => {
@@ -383,8 +383,8 @@ export default function HomeButtonMobile({ topOffset = '-40vh', marginTopSpacing
             );
           })}
           </div>
-          {/* 내부 하단 가로 구분선 */}
-          <div style={{ position: 'absolute', bottom: 0, left: -100, right: -100, borderTop: '1px solid #000000' }} />
+          {/* 내부 하단 가로 구분선 - 반응형으로 수정 */}
+          <div style={{ position: 'absolute', bottom: 0, left: 0, right: 0, borderTop: '1px solid #000000' }} />
         </div>
         {/* 모바일용 무한 텍스트 슬라이더 */}
         <div className="w-full py-12 bg-gray-100" style={{ marginTop: '30px' }}> 
